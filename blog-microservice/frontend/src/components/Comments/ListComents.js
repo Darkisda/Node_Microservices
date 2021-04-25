211a435a5b8216bc3d9cacb63bd0ca97a71e027e
Darkisda <luandesouzanascimento2011@gmail.com>
2021-04-25 17:01:30 -0300

    moderation, comment status and add new functionalities to event bus and query

--- blog-microservice/frontend/src/components/Comments/ListComents.js
+++ blog-microservice/frontend/src/components/Comments/ListComents.js
@@ -2,8 +2,28 @@ export default function ListComments({ comments }) {
   return (
     <ul>
       { comments.map( (comment ) => {
+
+        let content
+
+        switch (comment.status) {
+          case 'approved':
+            content = comment.content
+            break;
+        
+          case 'pending':
+            content = 'This comment is awating moderation'
+            break;
+
+          case 'rejected':
+            content = 'This comment has been rejected'
+            break;
+
+          default:
+            break;
+        }
+
         return (    
-          <li key={comment.id}>{comment.content}</li>
+          <li key={comment.id}>{content}</li>
         )})}
     </ul>
   )
